ee777892f64234a2af7492837793554f9dc10329
Badskhiff <50920980+Badskhiff@users.noreply.github.com>
2019-11-11 22:21:51 +0300

    Update vpc.tf

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -3,6 +3,7 @@ resource "aws_vpc" "test_vpc" {
   instance_tenancy                    = var.instance_tenancy
   enable_dns_support                  = var.enable_dns_support
   enable_dns_hostnames                = var.enable_dns_hostnames
+  
   tags {
     Name = var.name
   }
