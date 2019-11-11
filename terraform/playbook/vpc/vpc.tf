e396c3e1e932fd1c309e1b8d74a268e3788c3f9f
Badskhiff <50920980+Badskhiff@users.noreply.github.com>
2019-11-12 00:29:35 +0300

    Update vpc.tf

--- terraform/playbook/vpc/vpc.tf
+++ terraform/playbook/vpc/vpc.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "us-east-2"
-  shared_credentials_file = "${pathexpand("~/.aws/credentials")}
+  shared_credentials_file = pathexpand("~/.aws/credentials")
   #access_key = "${var.aws_access_key}"
   #secret_key = "${var.aws_secret_key}"
 }
